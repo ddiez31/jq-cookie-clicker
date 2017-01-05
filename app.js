4fccf07bb2712638d0e8a6edeec976f1cc797747
David <david.diez31@free.fr>
2017-01-05 16:07:57 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-	//votre code
+$(document).ready(function() {
+    //votre code
 
+    var counter = 0;
+    var counterInfini = 0;
+    var message = ["allez c'est bon!", "c'est bientôt fini?", "t'en n'a pas marre??", "retournes bosser!!"];
+
+
+    $("img").click(function() {
+        counterInfini++;
+        counter++
+        $("span").html(counterInfini);
+        if (counterInfini == 20) {
+            alert("N'as-tu rien d'autre à faire ??");
+            counter = 0;
+            console.log(counter);
+        } else(counterInfini == counterInfini + 20) {
+            alert("pfff");
+
+        }
+    });
 
 });
